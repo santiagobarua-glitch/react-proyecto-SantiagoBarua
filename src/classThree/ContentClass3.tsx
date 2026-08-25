9e60af0fdffa00d0b82e3f1ba540aac68ce8716b
Santiago Barua <santiago.barua@gm2dev.com>
2026-08-25 10:48:23 -0300

    feat: add Likes component with like button functionality

--- src/classThree/ContentClass3.tsx
+++ src/classThree/ContentClass3.tsx
@@ -1,9 +1,11 @@
 import Contador from "./Contador";
+import Likes from "./Likes";
 
 function Content() {
     return (
         <>
             <Contador />
+            <Likes />
         </>
     )
 }
